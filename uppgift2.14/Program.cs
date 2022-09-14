4261c4a411d833623fcb53c6b5f0a3d6089f67c1
Admin <Admin@TEB123013>
2022-09-14 11:44:54 +0200

    Add project files.

---
+++ uppgift2.14/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Uppgift_2_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ett decimaltal");
+            double Tal1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Skriv ett till decimaltal");
+            double Tal2 = double.Parse(Console.ReadLine());
+            
+
+            
+
+
+            double Produkt = Tal1 * Tal2;
+
+            Console.WriteLine("summan av talen gånger multiplicerat av varandra är " + Produkt);
+
+
+        }
+    }
+}
